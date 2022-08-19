a0127010f90943d9c85d428591e78df7adcf8e8e
ErickSilva2605 <erick.dev.it@gmail.com>
2022-08-19 01:52:32 -0300

    Delegando responsabilidade

--- app/src/main/java/br/com/alura/studentschedule/ui/ListStudentView.java
+++ app/src/main/java/br/com/alura/studentschedule/ui/ListStudentView.java
@@ -1,2 +1,61 @@
-package br.com.alura.studentschedule.ui;public class ListStudentView {
+package br.com.alura.studentschedule.ui;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+
+import br.com.alura.studentschedule.dao.StudentDAO;
+import br.com.alura.studentschedule.model.Student;
+import br.com.alura.studentschedule.ui.adapter.ListStudentAdapter;
+
+public class ListStudentView {
+
+    private static final String ALERT_TITLE_REMOVING_STUDENT = "Removing Student";
+    private static final String ALERT_MESSAGE_REMOVING_STUDENT = "Are you sure you want to remove the student?";
+    private static final String ALERT_POSITIVE_BUTTON_REMOVING_STUDENT = "Yes";
+    private static final String ALERT_NEGATIVE_BUTTON_REMOVING_STUDENT = "No";
+    private final Context context;
+    private final ListStudentAdapter adapter;
+    private final StudentDAO dao;
+
+    public ListStudentView(@NonNull Context context) {
+        this.context = context;
+        this.adapter = new ListStudentAdapter(this.context);
+        this.dao = new StudentDAO();
+    }
+
+
+    public void updateStudentList() {
+        adapter.update(dao.getAll());
+    }
+
+    public void confirmRemoval(@NonNull final MenuItem item) {
+        new AlertDialog
+                .Builder(context)
+                .setTitle(ALERT_TITLE_REMOVING_STUDENT)
+                .setMessage(ALERT_MESSAGE_REMOVING_STUDENT)
+                .setPositiveButton(ALERT_POSITIVE_BUTTON_REMOVING_STUDENT, (dialogInterface, i) -> positiveRemoveStudent(item))
+                .setNegativeButton(ALERT_NEGATIVE_BUTTON_REMOVING_STUDENT, null)
+                .show();
+    }
+
+    private void positiveRemoveStudent(@NonNull final MenuItem item) {
+        AdapterView.AdapterContextMenuInfo menuInfo =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        Student selectedStudent = adapter.getItem(menuInfo.position);
+        removeStudent(selectedStudent);
+    }
+
+    private void removeStudent(Student student) {
+        dao.remove(student);
+        adapter.remove(student);
+    }
+
+    public void configureAdapter(@NonNull ListView studentsListView) {
+        studentsListView.setAdapter(adapter);
+    }
 }
--- app/src/main/java/br/com/alura/studentschedule/ui/activity/MainActivity.java
+++ app/src/main/java/br/com/alura/studentschedule/ui/activity/MainActivity.java
@@ -2,13 +2,11 @@ package br.com.alura.studentschedule.ui.activity;
 
 import static br.com.alura.studentschedule.ui.activity.ConstantsActivities.KEY_SELECTED_STUDENT;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -18,27 +16,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import br.com.alura.studentschedule.R;
-import br.com.alura.studentschedule.dao.StudentDAO;
 import br.com.alura.studentschedule.model.Student;
-import br.com.alura.studentschedule.ui.adapter.ListStudentAdapter;
+import br.com.alura.studentschedule.ui.ListStudentView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String APPBAR_TITLE = "List of Students";
-    private static final String ALERT_TITLE_REMOVING_STUDENT = "Removing Student";
-    private static final String ALERT_MESSAGE_REMOVING_STUDENT = "Are you sure you want to remove the student?";
-    private static final String ALERT_POSITIVE_BUTTON_REMOVING_STUDENT = "Yes";
-    private static final String ALERT_NEGATIVE_BUTTON_REMOVING_STUDENT = "No";
-    private final StudentDAO dao = new StudentDAO();
-    private ListStudentAdapter adapter;
+    private final ListStudentView listStudentView =  new ListStudentView(this);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         setTitle(APPBAR_TITLE);
-
         configureFabAddStudent();
         ConfigureStudentList();
     }
@@ -53,25 +43,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.activity_main_menu_remove)
-            confirmRemoval(item);
+            listStudentView.confirmRemoval(item);
         return super.onContextItemSelected(item);
-    }
-
-    private void confirmRemoval(@NonNull final MenuItem item) {
-        new AlertDialog
-                .Builder(this)
-                .setTitle(ALERT_TITLE_REMOVING_STUDENT)
-                .setMessage(ALERT_MESSAGE_REMOVING_STUDENT)
-                .setPositiveButton(ALERT_POSITIVE_BUTTON_REMOVING_STUDENT, (dialogInterface, i) -> removeStudent(item))
-                .setNegativeButton(ALERT_NEGATIVE_BUTTON_REMOVING_STUDENT, null)
-                .show();
-    }
-
-    private void removeStudent(@NonNull final MenuItem item) {
-        AdapterView.AdapterContextMenuInfo menuInfo =
-                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        Student selectedStudent = adapter.getItem(menuInfo.position);
-        removeStudent(selectedStudent);
     }
 
     private void configureFabAddStudent() {
@@ -86,23 +59,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        updateStudentList();
-    }
-
-    private void updateStudentList() {
-        adapter.update(dao.getAll());
+        listStudentView.updateStudentList();
     }
 
     private void ConfigureStudentList() {
         ListView studentsListView = findViewById(R.id.activity_main_student_list);
-        configureAdapter(studentsListView);
+        listStudentView.configureAdapter(studentsListView);
         configureItemClickListener(studentsListView);
         registerForContextMenu(studentsListView);
-    }
-
-    private void configureAdapter(ListView studentsListView) {
-        adapter = new ListStudentAdapter(this);
-        studentsListView.setAdapter(adapter);
     }
 
     private void configureItemClickListener(ListView studentsListView) {
@@ -110,11 +74,6 @@ public class MainActivity extends AppCompatActivity {
             Student selectedStudent = (Student) adapterView.getItemAtPosition(position);
             openStudentFormActivityToEdit(selectedStudent);
         });
-    }
-
-    private void removeStudent(Student student) {
-        dao.remove(student);
-        adapter.remove(student);
     }
 
     private void openStudentFormActivityToEdit(Student student) {
